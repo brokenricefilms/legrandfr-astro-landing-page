918b6acb1e3e44246a8430bd8cc1dd86b192e4bb
Thuan Pham <thuanowa@gmail.com>
2023-08-21 23:12:28 +0700

    redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://legrandfr.com",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), compress()]
+  }), mdx(), sitemap()]
 });
